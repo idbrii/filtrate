86b9a97c73f66b156d0634c8303164be38af91d0
pydave <pydave@gmail.com>
2009-03-08 21:35:14 -0700

    Add file reading and writing code.
    Works almost entirely as requested!
    
    TODO:
    * add auto-run of regression tests
    * test with actual inputs

--- reg_runner/rr_main.py
+++ reg_runner/rr_main.py
@@ -3,17 +3,30 @@
 import wx
 import generated
 
+outputfile = 'out_tests_to_run.txt'
+
 class RegRunner(generated.rr_MainFrame):
     def __init__(self, *args, **kwds):
         generated.rr_MainFrame.__init__(self, *args, **kwds)
 
+    def __force_all_tests(self, value):
+        for checkbox in self.checkboxes.values():
+            checkbox.SetValue(value)
+
     def handle_disabletests(self, event): # wxGlade: rr_MainFrame.<event_handler>
+        self.__force_all_tests(False)
         event.Skip()
 
     def handle_enabletests(self, event): # wxGlade: rr_MainFrame.<event_handler>
+        self.__force_all_tests(True)
         event.Skip()
 
     def handle_execute(self, event): # wxGlade: rr_MainFrame.<event_handler>
+        out = open(outputfile, 'w')
+        for key in self.checkboxes.keys():
+            if self.checkboxes[key].GetValue():
+                out.write(key)
+        out.close()
         event.Skip()
 
 # end of class RegRunner
--- reg_runner/utility_forgen.py
+++ reg_runner/utility_forgen.py
@@ -1,19 +1,21 @@
 import wx
 
+test_list_file = 'sampletestlist.txt'
+
 def __create_content(self):
-    __create_checkboxes(self, 'buildtypes.txt' )
+    __create_checkboxes(self, test_list_file)
 
 def __create_checkboxes(self, filename):
     """
     __create_checkboxes(filename) --> None
     Creates the checkboxes according to filename and adds them to self
     """
-    self.checkboxes = []
-    for text in ("some reg test", "another test"):
-        self.checkboxes.append( wx.CheckBox(self, -1, text) )
+    self.checkboxes = {}
+    for text in open(filename):
+        self.checkboxes[text] = ( wx.CheckBox(self, -1, text) )
         pass
 
 def add_dynamic_checkboxes(self, sizer):
     __create_content(self)
-    for checkbox in self.checkboxes:
+    for checkbox in self.checkboxes.values():
         sizer.Add(checkbox, 0, 0, 0)
